0e7f8f97f79381b9c39965f94a49cd7e526fd40b
Kevin Lamontagne <kevin.lamontagne@libeo.com>
2013-01-01 17:52:24 -0500

    Move around server attributes

--- attributes/default.rb
+++ attributes/default.rb
@@ -24,12 +24,3 @@ default['burp']['umask'] = '0027'
 
 default['burp']['ca_server_name'] = 'backup.libeo.com'
 
-#Server-only settings
-#default['burp']['server_only'] = {}
-default['burp']['server_only']['restore_client'] = [ 'localclient' ]
-default['burp']['server_only']['ratelimit_server'] = 'no'
-default['burp']['server_only']['ratelimit_server_mbps'] = '5'
-default['burp']['server_only']['notify_success_mail'] = 'kevin.lamontagne@libeo.com'
-default['burp']['server_only']['notify_failure_mail'] = 'kevin.lamontagne@libeo.com'
-
-
---
+++ attributes/server.rb
@@ -0,0 +1,7 @@
+#Server-only settings
+default['burp']['server_only']['restore_client'] = []
+default['burp']['server_only']['ratelimit_server'] = 'no'
+default['burp']['server_only']['ratelimit_server_mbps'] = '5'
+default['burp']['server_only']['notify_success_mail'] = 'kevin.lamontagne@libeo.com'
+default['burp']['server_only']['notify_failure_mail'] = 'kevin.lamontagne@libeo.com'
+
