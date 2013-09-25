30b4cddf90e9019b26d96954bf0c2ebe929670cd
Kevin Lamontagne <kevin@demarque.com>
2013-09-25 15:03:36 -0400

    fix non-root pid file for server on Ubuntu

--- attributes/server.rb
+++ attributes/server.rb
@@ -2,6 +2,7 @@
 default['burp']['server_only']['client_can_force_backup'] = false
 default['burp']['server_only']['restrict_to_same_environment'] = false
 default['burp']['server_only']['hardlinked_archive'] = true
+default['burp']['server_only']['pidfile'] = "/var/lock/burp-server.pid" # server not running as root... can't write to /var/run !
 default['burp']['server_only']['restore_client'] = []
 default['burp']['server_only']['ratelimit_server'] = 'no'
 default['burp']['server_only']['ratelimit_server_mbps'] = '5'
