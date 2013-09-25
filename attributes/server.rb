12e44fb96c3e56d95932ad63bb3eecd33bcb3b48
Kevin Lamontagne <kevin@demarque.com>
2013-09-25 15:02:58 -0400

    make hardlinked_archive an attribute

--- attributes/server.rb
+++ attributes/server.rb
@@ -1,6 +1,7 @@
 #Server-only settings
 default['burp']['server_only']['client_can_force_backup'] = false
 default['burp']['server_only']['restrict_to_same_environment'] = false
+default['burp']['server_only']['hardlinked_archive'] = true
 default['burp']['server_only']['restore_client'] = []
 default['burp']['server_only']['ratelimit_server'] = 'no'
 default['burp']['server_only']['ratelimit_server_mbps'] = '5'
