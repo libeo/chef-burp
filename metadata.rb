c86e2543eb53e8962fb5b5d20f9bb86d41e179e9
Kevin Lamontagne <kevin@demarque.com>
2014-03-17 15:32:22 -0400

    Bump version -> 0.11.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "sysadmins@libeo.com"
 license          "All rights reserved"
 description      "Installs/Configures burp"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.10.0"
+version          "0.11.0"
 
 supports 'debian', ">= 6.0"
 supports 'ubuntu', ">= 12.04"
