b66cc34367961fd8c57475a5dc2f40ed63441f60
Kevin Lamontagne <kevin@demarque.com>
2014-08-18 14:02:52 -0400

    MIT License

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             "burp"
 maintainer       "Libeo"
 maintainer_email "sysadmins@libeo.com"
-license          "All rights reserved"
+license          "MIT"
 description      "Installs/Configures burp"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.7.0"
